678743e66a04532fc01fde9f95cf4037cf4b922b
Alejandro <o.i.velykyy@student.khai.edu>
2025-05-30 22:07:01 +0300

    Add styles for form

--- src/main.js
+++ src/main.js
@@ -3,6 +3,6 @@ $(document).ready(function () {
     $('#add').click(function () {
         let label = $('#Label').val();
         let note = $('#note').val();
-        $('.Notes').append(`<h1>${label}</h1><br><p>${note}</p><br>`);
+        $('.Notes').append(`<h1 class="note_label">${label}</h1><br><p class="noted_text">${note}</p><br>`);
     })
 });
