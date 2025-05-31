8c0a0a2286c586ee887048020c559dbfd48b96e6
Alejandro <o.i.velykyy@student.khai.edu>
2025-05-31 22:46:07 +0300

    archivation and removing works

--- src/main.js
+++ src/main.js
@@ -1,8 +1,42 @@
 $(document).ready(function () {
-
+    let num = 0;
     $('#add').click(function () {
         let label = $('#Label').val();
         let note = $('#note').val();
-        $('.Notes').append(`<h1>${label}</h1><br><p>${note}</p><br>`);
-    })
+        if(label == ""){
+            alert("Enter something in field");
+        }
+        else {
+            $('.Notes').append(`<div id="note_block">
+            <h1 class="note_label">${label}</h1>
+            <p class="noted_text">${note}</p><br>
+            <div class="buttons">
+            <button id="archive">Archive</button>
+            <button id="remove">Remove</button>
+            </div>
+            </div>`);
+            num++;
+        }
+        $('.Notes').on('click', '#remove', function () {
+            if(archive == false) {
+                $(this).closest('#note_block').remove();
+                num--;
+            }
+        });
+        $('.Notes').on('click', '#archive', function () {
+            if(archive == false) {
+                archive = true;
+                $(this).closest('#note_block').css('background-color', 'lightgray');
+                $(this).closest('#note_block').find('.note_label').css('text-decoration', 'line-through');
+                $(this).closest('#note_block').find('.noted_text').css('text-decoration', 'line-through');
+            }
+            else {
+                archive = false;
+                $(this).closest('#note_block').css('background-color', 'white');
+                $(this).closest('#note_block').find('.note_label').css('text-decoration', 'none');
+                $(this).closest('#note_block').find('.noted_text').css('text-decoration', 'none');
+            }})
+    });
+
 });
+
