10bd50ee7154f12274afb2d9a8f8269721df1f64
unknown <robocodeuserpro@gmail.com>
2025-05-31 12:49:11 +0300

    list save objects

--- src/main.js
+++ src/main.js
@@ -1,13 +1,15 @@
 $(document).ready(function () {
+    let list = [];
 
     $('#add').click(function () {
         let label = $('#Label').val();
         let note = $('#note').val();
         if(label == ""){
-            alert("bububu");
+            alert("Enter something in field");
         }
         else {
             $('.Notes').append(`<div class="note"><h1 class="note_label">${label}</h1><br><p class="noted_text">${note}</p><br></div>`);
+            list.push(0);
         }
     })
 });
